ee97cf3f87cd28b323f30d89d0f4c0deac847994
eLViO4 <valeriyliubimyi@gmail.com>
2025-03-24 16:41:47 +0200

    Initial commit!

--- src/main/java/org/example/ControllerTest/TaskControllerTest.java
+++ src/main/java/org/example/ControllerTest/TaskControllerTest.java
@@ -6,70 +6,7 @@ import org.example.Service.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-/*
-@Controller
-@RequestMapping("/task")
-@RequiredArgsConstructor
-public class TaskController {
-    private final TaskService taskService;
 
-    @GetMapping("/{id}")
-    public String getTask(@PathVariable Long id, Model model) {
-        try {
-            Task task = taskService.findTaskById(id);
-            model.addAttribute("task", task);
-            return "todo-tasks";
-        } catch (Exception e) {
-            return "redirect:/not-found";
-        }
-    }
-
-    @PostMapping("/create")
-    public String createTask(@ModelAttribute Task task) {
-        try {
-            taskService.createTask(task);
-            return "redirect:/todo-tasks";
-        } catch (Exception e) {
-            return "redirect:/error";
-        }
-    }
-
-    @PutMapping("/{id}")
-    public String updateTask(@PathVariable("id") long id, @ModelAttribute Task task) {
-        try {
-            task.setId(id);
-            taskService.updateTask(task);
-            return "redirect:/todo-tasks";
-        } catch (Exception e) {
-            return "redirect:/error";
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteTask(@PathVariable("id") long id) {
-        try {
-            taskService.deleteTask(id);
-            return "redirect:/todo-tasks";
-        } catch (Exception e) {
-            return "redirect:/not-found";
-        }
-    }
-
-    @GetMapping
-    public String getAllTasks(Model model) {
-        List<Task> tasks = taskService.getAllTasks();
-        model.addAttribute("tasks", tasks);
-        return "todo-tasks";
-    }
-
-    @GetMapping("/todo/{todoId}")
-    public String getByToDoId(@PathVariable long todoId, Model model) {
-        List<Task> tasks = taskService.getByToDoId(todoId);
-        model.addAttribute("tasks", tasks);
-        return "todo-tasks";
-    }
-}
-*/
 @RestController
 @RequestMapping("/task")
 @RequiredArgsConstructor
